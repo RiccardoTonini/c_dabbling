a9de4e13d4071ecce7ce05f561a9c08817cbf122
Riccardo Tonini <riccardo.tonini@gmail.com>
2021-05-30 14:36:16 +0100

    Float arithmetic

--- the_c_programming_language/fahrenheit_to_celsius.c
+++ the_c_programming_language/fahrenheit_to_celsius.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char const *argv[])
 {
-	int fahr, celsius;
+	float fahr, celsius;
 	int lower_bound, upper_bound, step;
 
 	lower_bound = 0;
@@ -12,9 +12,14 @@ int main(int argc, char const *argv[])
 
 	fahr = lower_bound;
 
+	printf("Fahreneit to Celsius Conversion Table \n");
+
 	while (fahr <= upper_bound) {
-		celsius = 5 * (fahr - 32) / 9;
-		printf("%3d°F\t %6d°C\n", fahr, celsius);
+		// celsius = 5 * (fahr - 32) / 9;
+		celsius = (5.0 / 9.0) * (fahr - 32.0);
+		// %3d Augment each %d with a width of 3 so that numbers
+		// will be printed right-justified in their fields
+		printf("%3.0f°F\t %6.1f°C\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 
